19c8c1f8ad924fe9f3d4b858ae2a8d5c5f3a4238
Vijayshree <vijayshree.joshi.2020@uni.strath.ac.uk>
2021-03-19 11:41:15 +0000

    create new data model  and data utility class and modified prototype in notes page

---
+++ app/src/main/java/com/example/bookbuzz/BookBuzzDataModel.java
@@ -0,0 +1,40 @@
+package com.example.bookbuzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookBuzzDataModel {
+    private String isbn;
+    private String currentBookName;
+    private List<String> notes;
+
+    public BookBuzzDataModel(final String bookName, final String bookIsbn) {
+        this.currentBookName = bookName;
+        this.isbn = bookIsbn;
+        notes = new ArrayList<>();
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCurrentBookName() {
+        return currentBookName;
+    }
+
+    public void setCurrentBookName(String currentBookName) {
+        this.currentBookName = currentBookName;
+    }
+
+    public List<String> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<String> notes) {
+        this.notes = notes;
+    }
+}
