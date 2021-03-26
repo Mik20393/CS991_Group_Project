008a915542d6faf2c1f0e5118397576806290b74
Daryl Whitmore <whitmore.daryl@gmail.com>
2021-03-26 18:06:48 +0000

    added linear layouts to align buttons

--- app/src/main/java/com/example/bookbuzz/MainActivity.java
+++ app/src/main/java/com/example/bookbuzz/MainActivity.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationMenu;
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 
 public class MainActivity extends AppCompatActivity {
 
